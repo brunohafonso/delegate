92b9dacf170768f7f7bca36b8a827b56707f6c43
BRUNO HENRIQUE AFONSO <44063076806@jpmorgan.sp>
2017-11-22 21:57:22 -0200

    salvando tudo

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace projeto_delegate
+{
+    class Program
+    {
+
+        public delegate void Cortador(string queijo);
+
+        static void Main(string[] args)
+        {
+            Cortador vera = new Cortador(DataAbreviada);
+            vera("OLá, eu sou um delegate");
+            Cortador filha = new Cortador(DataCompleta);
+            filha("Bruno");  
+        }
+
+        static void DataAbreviada(string texto)
+        {
+            System.Console.WriteLine("{0}, hoje é {1}", texto, DateTime.Now.ToShortDateString());
+        }
+
+        static void DataCompleta(string nome)
+        {
+            System.Console.WriteLine("Olá {0}, hoje é {1}", nome, DateTime.Now.ToLongDateString()); 
+        }
+    }
+}
